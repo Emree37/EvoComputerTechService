39f92387d692213c29609dfdb1f6544a8bdcf04e
Emree37 <34719304+Emree37@users.noreply.github.com>
2022-02-12 10:14:11 +0300

    Role Assign

--- EvoComputerTechService/Areas/Admin/Controllers/AdminController.cs
+++ EvoComputerTechService/Areas/Admin/Controllers/AdminController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using EvoComputerTechService.Models.Identity;
+using EvoComputerTechService.ViewModels;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,6 +11,15 @@ namespace EvoComputerTechService.Areas.Admin.Controllers
 {
     public class AdminController : AdminBaseController
     {
+        private readonly RoleManager<ApplicationRole> _roleManager;
+        private readonly UserManager<ApplicationUser> _userManager;
+
+        public AdminController(RoleManager<ApplicationRole> roleManager, UserManager<ApplicationUser> userManager)
+        {
+            _roleManager = roleManager;
+            _userManager = userManager;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -16,6 +28,35 @@ namespace EvoComputerTechService.Areas.Admin.Controllers
         public IActionResult Users()
         {
             return View();
+        }
+
+        public async Task<IActionResult> RoleAssign(string id)
+        {
+            ApplicationUser user = await _userManager.FindByIdAsync(id);
+            List<ApplicationRole> allRoles = _roleManager.Roles.ToList();
+            List<string> userRoles = await _userManager.GetRolesAsync(user) as List<string>;
+            List<RoleAssignViewModel> assignRoles = new List<RoleAssignViewModel>();
+            allRoles.ForEach(role => assignRoles.Add(new RoleAssignViewModel
+            {
+                HasAssign = userRoles.Contains(role.Name),
+                RoleId = role.Id,
+                RoleName = role.Name
+            }));
+
+            return View(assignRoles);
+        }
+        [HttpPost]
+        public async Task<ActionResult> RoleAssign(List<RoleAssignViewModel> modelList, string id)
+        {
+            ApplicationUser user = await _userManager.FindByIdAsync(id);
+            foreach (RoleAssignViewModel role in modelList)
+            {
+                if (role.HasAssign)
+                    await _userManager.AddToRoleAsync(user, role.RoleName);
+                else
+                    await _userManager.RemoveFromRoleAsync(user, role.RoleName);
+            }
+            return RedirectToAction("Users", "Admin");
         }
     }
 }
