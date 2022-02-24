2155f44829994c88a5e16ba2c6d0121c2203dcd9
Emree37 <34719304+Emree37@users.noreply.github.com>
2022-02-24 14:50:47 +0300

    Edit Views

--- EvoComputerTechService/Controllers/AccountController.cs
+++ EvoComputerTechService/Controllers/AccountController.cs
@@ -202,7 +202,7 @@ namespace EvoComputerTechService.Controllers
         public async Task<IActionResult> Logout()
         {
             await _signInManager.SignOutAsync();
-            return RedirectToAction("Index", "Home");
+            return RedirectToAction("Login", "Account");
         }
 
         
